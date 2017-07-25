ad8152d9e5717ee9d6713b79e9493be1b7ea4e09
sathish15g <gsathishkumar15@gmail.com>
2017-07-25 17:28:18 +0530

    initial product app using default exchange and node js

---
+++ product node-js/product-nodejs-app/service/product-service.js
@@ -0,0 +1,56 @@
+
+var Product=require("../model/Product");
+var HashMap = require('hashmap');
+var rabbitmqconn = require('../mess/rabbitmqconnection');
+var productrepository={
+		
+		productMap: null,
+		
+		initializeMap: function(){
+			console.log("Initializing the hash map");
+			this.productMap=new HashMap();
+		},
+
+				
+		saveProduct:function(product){
+			console.log("save product is called"+product);
+            rabbitmqconn.connectMQ(product.productId);
+			this.productMap.set(product.productId,JSON.stringify(product));
+			
+		},
+		
+		getProduct:function(productId){
+			console.log("get product is called"+productId);
+			return this.productMap.get(productId);
+		}
+};
+productrepository.initializeMap();
+
+var productservice={
+		
+		productRepo: null,
+		
+		setProductRepo: function(repo){
+			this.productRepo=repo;
+		},
+		
+		getProductRepo:function(){
+			return this.productRepo;
+		},
+
+		saveProduct:function(product){
+			console.log("save product service is called"+product);
+			this.getProductRepo().saveProduct(product);
+		},
+
+		getProduct:function(productId){
+			console.log("get product service is called"+productId);
+			return this.getProductRepo().getProduct(productId);
+		}
+		
+		
+};
+
+productservice.setProductRepo(productrepository);
+
+module.exports=productservice;
